b4449449141528ffcc713b4459a92b5d3bcb87dd
zhihaofans <zhihaofans@hotmail.com>
2022-02-25 11:27:17 +0800

    更新 [name].vue

--- src/pages/hi/[name].vue
+++ src/pages/hi/[name].vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 const props = defineProps<{ name: string }>()
 const router = useRouter()
+import { NButton } from 'naive-ui'
 </script>
 
 <template>
@@ -20,6 +21,7 @@ const router = useRouter()
       >
         Back
       </button>
+      <n-button @click="router.back()">返回</n-button>
     </div>
   </div>
 </template>
